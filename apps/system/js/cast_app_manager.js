1cbf342de50311b3b14db2dc7d0b2f2a5962aae5
WangXianfeng <wangxianfeng@infthink.com>
2014-10-31 11:50:37 +0800

    Update new native app start protocol.

--- apps/system/js/cast_app_manager.js
+++ apps/system/js/cast_app_manager.js
@@ -1,6 +1,6 @@
-'used dict'
+'use strict';
+
 var CastAppManager = (function () {
-  var prefix = 'app://';
 
   var castAppConfig = {
     'isActivity': false,
@@ -19,54 +19,70 @@ var CastAppManager = (function () {
 
   function doAppCommand(message) {
     console.log('pal:', 'message = ' + JSON.stringify(message));
-    //{"type":"LAUNCH_RECEIVER","app_id":"~browser",
-    // "app_info":{"url":"http://castapp.infthink.com/receiver/mediaplayer/index.html",
-    // "useIpc":true}}
-
     var command = message.type;
     if (command == 'LAUNCH_RECEIVER') {
       if (message.app_id == '~browser') {
-        console.log('XXXXX appinfo');
-        startCastAppContainer(message.app_info.url);
+        if (!message.app_info) {
+          return;
+        }
+        var httpStr = message.app_info.url;
+        if (httpStr.indexOf('http') == 0) {
+          startCastAppContainer(httpStr);
+        }
       } else if (message.app_id == '~native') {
-        console.log('XXXXX packagename');
-        startLocalApp(message.app_info.package_name);
+        if (!message.app_info) {
+          return;
+        }
+        var appStr = message.app_info.url;
+        if (appStr.index('app:?') == 0) {
+          startLocalApp(appStr.slice(5));
+        } else {
+          console.error('can not start native application: ' + appStr);
+        }
       } else if (message.appUrl) {
-        //for node-castd
         startCastAppContainer(message.appUrl);
       }
     } else if (command == 'STOP_RECEIVER') {
-
-      if (message.app_info.package_name) {
-        localAppConfig.url = prefix + message.app_info.package_name + '/index.html';
-        localAppConfig.manifestURL = prefix + message.app_info.package_name + '/manifest.webapp';
-        localAppConfig.origin = prefix + message.app_info.package_name;
-        stopApplication(localAppConfig);
+      if (message.app_id == '~browser' || message.appUrl) {
+        stopApplication(castAppConfig);
+      } else if (message.app_id == '~native') {
+        if (message.app_info && message.app_info.url) {
+          var nativeStr = message.app_info.url;
+          if (nativeStr.index('app:?') == 0) {
+            var appPkg = nativeStr.slice(5);
+            var prefix = 'app://';
+            console.log('native app package name: ' + appPkg);
+            localAppConfig.url = prefix + appPkg + '/index.html';
+            localAppConfig.manifestURL = prefix + appPkg + '/manifest.webapp';
+            localAppConfig.origin = prefix + appPkg;
+            stopApplication(localAppConfig);
+          } else {
+            console.error('can not stop native application: ' + nativeStr);
+          }
+        }
       }
-      stopApplication(castAppConfig);
-
+    } else {
+      console.error('not support command: ' + command);
     }
-
-
   }
-
 
   function stopApplication(config) {
     console.log('pal:', 'Stop Receiver App!');
     AppWindowManager.display(null, 'immediate', 'immediate');
-    appWindowFactory.handleEvent({
-      type: 'webapps-close',
-      detail: config
-    });
+    AppWindowManager.kill(config.origin);
+    //TODO
+//    appWindowFactory.handleEvent({
+//      type: 'webapps-close',
+//      detail: config
+//    });
   }
 
   function startLocalApp(pkg) {
-    //com.a.com
+    console.log('package name = ' + pkg);
     var prefix = 'app://';
     localAppConfig.url = prefix + pkg + '/index.html';
     localAppConfig.manifestURL = prefix + pkg + '/manifest.webapp';
     localAppConfig.origin = prefix + pkg;
-
     appWindowFactory.handleEvent({
       type: 'webapps-launch',
       detail: localAppConfig
@@ -75,14 +91,18 @@ var CastAppManager = (function () {
   }
 
   function startCastAppContainer(appUrl) {
-    appWindowFactory.handleEvent({
-      type: 'webapps-launch',
-      detail: castAppConfig
-    });
+    navigator.mozApps.mgmt.getAll().onsuccess = function (event) {
+      var apps;
+      apps = event.target.result;
+      apps.forEach(function (app) {
+        if (app.manifest.name === 'CastAppContainer') {
+          app.launch();
+        }
+      });
+    };
     console.log('pal:', 'startCastAppContainer:', appUrl);
     setAppUrl(['launch', appUrl], 'pal-app-cmd');
   }
-
 
   function setAppUrl(data, cmd) {
     console.log('pal:', 'setAppUrl! the data is ' + data + ' , the command is ' + cmd);
@@ -105,7 +125,6 @@ var CastAppManager = (function () {
       }
     };
   }
-
 
   return {
     doAppCommand: doAppCommand
