05ec3f7b2d1591c5f15ab95da9cee75a2d44c1df
terry3 <terryr3rd@yeah.net>
2016-01-31 15:06:00 +0800

    Add timer and configurable ip address.

--- apps/rtsp-player/player.js
+++ apps/rtsp-player/player.js
@@ -6,15 +6,54 @@
   var mediaElement;
   var label;
 
+
+  function formatTime(digit) {
+    if (digit >=0 && digit < 10) {
+      return "0" + digit.toString();
+    }
+    return digit.toString();
+  }
+
+  function startTimer() {
+    var timeNow = timer.innerHTML;
+    var timerArray = timeNow.split(":");
+    var seconds = parseInt(timerArray[2]);
+    var minutes = parseInt(timerArray[1]);
+    var hours = parseInt(timerArray[0]);
+    seconds += 1;
+    // seconds
+    if (seconds >= 60) {
+      seconds = 0;
+      minutes += 1;
+    }
+    // minutes
+    if (minutes >= 60) {
+      minutes = 0;
+      hours += 1;
+    }
+    timerArray[0] = formatTime(hours);
+    timerArray[1] = formatTime(minutes);
+    timerArray[2] = formatTime(seconds);
+
+    timer.innerHTML = timerArray[0] + ":" + timerArray[1] + ":" + timerArray[2];
+    var t = setTimeout(startTimer, 1000);
+  }
+
   window.onload = function () {
     console.log("XXXXX window onload");
     mediaElement = document.getElementById('media');
     label = document.getElementById('label');
+    timer = document.getElementById('timer');
 
     document.getElementById('btn').addEventListener('click', function (event) {
-      mediaElement.src = document.getElementById('ipaddr').value;
+      var ipport = document.getElementById('ipaddr').value;
+      mediaElement.src = "rtsp://" + ipport + "/live";
+      var result = ipport.split(":");
+      document.getElementById("output_ip").innerHTML = result[0];
+      document.getElementById("output_port").innerHTML = result[1];
+      startTimer();
     });
-//    label.innerHTML = "PAGE READY " + (new Date()).toString();
+
     mediaElement.addEventListener('error', function (event) {
       label.innerHTML = "error";
     });
